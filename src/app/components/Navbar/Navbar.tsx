0e6f9ec879276b18fa127d60e7e15b192e1b2531
Mauricio <mauriciorojascareers@gmail.com>
2024-12-05 21:37:32 -0600

    restructure page.ts

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="z-10 p-4 absolute top-0 font-extralight text-white text-xs md:text-xl sm:flex items-center justify-center md:block hidden">
+      Phoenix Lidar Systems
+    </div>
+  );
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import CesiumWrapper from "./components/CesiumWrapper";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <CesiumWrapper />
-      <div className="z-10 p-4 absolute top-0 font-extralight text-white text-xs md:text-xl sm:flex items-center justify-center md:block hidden">
-        Phoenix Lidar Systems
-      </div>
     </main>
   );
 }
