48e81a546787b2eab65f46e03a84c0a89b3aa5f5
Mauricio <mauriciorojascareers@gmail.com>
2024-12-29 00:54:25 -0600

    blue bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body>
         <SidebarProvider>
           <AppSidebar />
-          <main className="w-full h-[100vh] bg-blue-800 relative">
+          <main className="w-full h-[100vh] relative">
             <header className="absolute top-0 left-0 z-50 w-full p-2">
               {/* <SidebarTrigger variant="secondary" /> */}
             </header>
