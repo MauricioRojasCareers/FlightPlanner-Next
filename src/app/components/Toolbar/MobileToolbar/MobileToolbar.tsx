238f5fd049f5dca7695a174dfe0037640f5a4490
Mauricio <mauriciorojascareers@gmail.com>
2024-12-29 13:43:57 -0600

    h-svh

--- src/app/components/Toolbar/MobileToolbar/MobileToolbar.tsx
+++ src/app/components/Toolbar/MobileToolbar/MobileToolbar.tsx
@@ -21,8 +21,8 @@ const MobileToolbar: FunctionComponent<ToolbarProps> = ({
 }) => {
   return (
     <>
-      <div className="w-full h-screen bg-red-300 flex flex-row relative">
-        <div className="bg-yellow-200 w-full h-screen justify-between gap-2 flex p-4 flex-col">
+      <div className="w-full h-svh bg-red-300 flex flex-row relative">
+        <div className="bg-yellow-200 w-full h-svh justify-between gap-2 flex p-4 flex-col">
           {/* Top Section */}
           <div className="bg-blue-300 flex">
             <MenuButton onClick={() => resetView()} />
