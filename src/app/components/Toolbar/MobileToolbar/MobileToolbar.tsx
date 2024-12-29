abd2d70f31728dc30f752dc66e8e29121ddd706a
Mauricio <mauriciorojascareers@gmail.com>
2024-12-29 13:18:53 -0600

    restructure div for mobile toolbar

--- src/app/components/Toolbar/MobileToolbar/MobileToolbar.tsx
+++ src/app/components/Toolbar/MobileToolbar/MobileToolbar.tsx
@@ -21,33 +21,35 @@ const MobileToolbar: FunctionComponent<ToolbarProps> = ({
 }) => {
   return (
     <>
-      <div className="w-full min-h-screen flex flex-row relative">
-        <div className="bg-yellow-200 w-full justify-between gap-2 flex absolute p-4">
-          <MenuButton
-            onClick={() => {
-              resetView();
-            }}
-          />
-          <OpenMissionButton onClick={resetView} />
-          {/* Search Bar */}
-          <div className="flex items-center w-full">
-            <SearchBar />
-          </div>
+      <div className="w-full h-screen bg-red-300 flex flex-row relative">
+        <div className="bg-yellow-200 w-full justify-between gap-2 flex absolute p-4 h-full flex-col">
+          <div className="bg-blue-300 flex">
+            <MenuButton
+              onClick={() => {
+                resetView();
+              }}
+            />
+            <OpenMissionButton onClick={resetView} />
+            {/* Search Bar */}
+            <div className="flex items-center w-full">
+              <SearchBar />
+            </div>
 
-          {/* Menu Button */}
-          <div className="flex justify-end h-full gap-2 items-center ">
-            <DrawButton onClick={resetView} />
-            <TerrainButton onClick={tiltView} />
+            {/* Menu Button */}
+            <div className="flex justify-end h-full gap-2 items-center ">
+              <DrawButton onClick={resetView} />
+              <TerrainButton onClick={tiltView} />
+            </div>
           </div>
-        </div>
-        <div className="absolute bottom-0 flex w-full justify-between p-4 mb-20">
-          {/* NorthButton anchored in the bottom-risght corner */}
-          <div className="">
-            <YourLocation onClick={resetView} />
-          </div>
-          {/* NorthButton anchored in the bottom-right corner */}
-          <div className="">
-            <GlobeView onClick={globeView} />
+          <div className="bg-white flex justify-between">
+            {/* NorthButton anchored in the bottom-risght corner */}
+            <div className="">
+              <YourLocation onClick={resetView} />
+            </div>
+            {/* NorthButton anchored in the bottom-right corner */}
+            <div className="">
+              <GlobeView onClick={globeView} />
+            </div>
           </div>
         </div>
       </div>
