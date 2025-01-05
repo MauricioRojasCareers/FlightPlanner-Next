9cd20601007f93c1dafe80c55ac0f0f3a63da921
Mauricio <mauriciorojascareers@gmail.com>
2025-01-04 20:06:23 -0600

    import fix

--- src/app/components/CesiumWrapper.tsx
+++ src/app/components/CesiumWrapper.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 import React from "react";
-import type { CesiumType } from "../types/cesium";
+import type { CesiumType } from "@/types/cesium";
 import Loading from "@/app/components/Loading";
 import { Toaster } from "../../components/ui/toaster";
 
